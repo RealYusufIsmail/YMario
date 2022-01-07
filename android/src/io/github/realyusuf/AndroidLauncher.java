830f0e828e2021cdbcaf31d5f6b8f4528cebe4ea
Yusuf <yusufgamer222@gmail.com>
2022-01-07 22:01:28 +0000

    getting started

---
+++ android/src/io/github/realyusuf/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.github.realyusuf;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.realyusuf.YMario;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new YMario(), config);
+	}
+}
---
+++ desktop/src/io/github/realyusuf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.realyusuf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.realyusuf.YMario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YMario(), config);
+	}
+}
