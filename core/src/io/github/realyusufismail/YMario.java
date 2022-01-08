20ca54a7571c30135c1d8d15d7bb9ac1e63bd873
Yusuf <yusufgamer222@gmail.com>
2022-01-08 20:35:34 +0000

    Added table.
    Added score,time,level and more

--- core/src/io/github/realyusufismail/YMario.java
+++ core/src/io/github/realyusufismail/YMario.java
@@ -1,26 +1,25 @@
 package io.github.realyusufismail;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import io.github.realyusufismail.screens.PlayScreen;
 
-public class YMario extends ApplicationAdapter {
-    SpriteBatch batch;
+public class YMario extends Game {
+    public static final int V_WIDTH = 400;
+    public static final int V_HEIGHT = 208;
+    public SpriteBatch batch;
     Texture img;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        setScreen(new PlayScreen(this));
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        super.render();
     }
 
     @Override
---
+++ core/src/io/github/realyusufismail/screens/PlayScreen.java
@@ -0,0 +1,90 @@
+package io.github.realyusufismail.screens;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import io.github.realyusufismail.YMario;
+import io.github.realyusufismail.scenes.Hud;
+
+public class PlayScreen implements Screen {
+    private final YMario game;
+    private final OrthographicCamera gameCamera;
+    private final Viewport gamePort;
+    private Hud hud;
+
+    public PlayScreen(YMario game) {
+        this.game = game;
+        gameCamera = new OrthographicCamera();
+        gamePort = new FitViewport(YMario.V_WIDTH, YMario.V_HEIGHT, gameCamera);
+        hud = new Hud(game.batch);
+    }
+
+    /**
+     * Called when this screen becomes the current screen for a {@link Game}.
+     */
+    @Override
+    public void show() {
+
+    }
+
+    /**
+     * Called when the screen should render itself.
+     *
+     * @param delta The time in seconds since the last render.
+     */
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    /**
+     * @param width the new width
+     * @param height the new height
+     * @see ApplicationListener#resize(int, int)
+     */
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height); // update viewport size
+    }
+
+    /**
+     * @see ApplicationListener#pause()
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /**
+     * @see ApplicationListener#resume()
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /**
+     * Called when this screen is no longer the current screen for a {@link Game}.
+     */
+    @Override
+    public void hide() {
+
+    }
+
+    /**
+     * Called when this screen should release all resources.
+     */
+    @Override
+    public void dispose() {
+
+    }
+}
